e6b85750e90312173c7e04166e168cd7b8c453d2
limayeShrikant <shrikant.limaye89@gmail.com>
2024-09-17 10:07:22 +0530

    Made changes in Vite Config

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const apikey = process.env.VITE_API_KEY;
+
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '5f5a42a55990489ab0b041327063caa7'
+        key: apikey
     }
 })
